2cd3700dd1dcd7df9bc8af473b06c798cce798e2
Noriaki Uchiyama <uchiyama.noriaki@gmail.com>
2021-08-28 06:25:55 +0900

    init eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  extends: ['@cybozu/eslint-config/presets/node-typescript-prettier'],
+  // settings: {
+  //   'import/resolver': {
+  //     'babel-module': {
+  //       alias: {
+  //         '~': './src/',
+  //       },
+  //     },
+  //   },
+  // },
+};
